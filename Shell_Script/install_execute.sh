c69f9822bafb41077ba80db73a0da80f4b99b571
jangsungLee <hoan12340@naver.com>
2020-06-26 21:03:21 +0900

    Update install_execute.sh

--- Shell_Script/install_execute.sh
+++ Shell_Script/install_execute.sh
@@ -9,12 +9,12 @@ fi
 
 mkdir /home/pi/doorLock
 cd /home/pi/doorLock
-wget https://raw.githubusercontent.com/jangsungLee/SmartFarm-IoT-System/Shell_Script/master/install-qt_build_environment.sh
+wget https://raw.githubusercontent.com/jangsungLee/SmartFarm-IoT-System/master/Shell_Script/install-qt_build_environment.sh
 chmod +x install-qt_build_environment.sh
 sudo ./install-qt_build_environment.sh
 
 echo "중재자 IP를 ${MEDIATOR_IP_ADDRESS}으로 설정합니다."
 
-wget https://github.com/jangsungLee/SmartFarm-IoT-System/Shell_Script/raw/master/doorLock_System
+wget https://github.com/jangsungLee/SmartFarm-IoT-System/Shell_Script/raw/master/QT_Project/doorLock_System
 chmod +x doorLock_System
 sudo sed -i 's/@xscreensaver -no-splash/@sudo \/home\/pi\/doorLock\/doorLock_System '"$MEDIATOR_IP_ADDRESS"'\n@xscreensaver -no-splash/g' /etc/xdg/lxsession/LXDE-pi/autostart
